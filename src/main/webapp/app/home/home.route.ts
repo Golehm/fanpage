5d9bd34f611a48bc39633ac7c802dfc6a26f5f48
Golehm <grzegorz.gorski@yahoo.pl>
2020-12-07 21:11:50 +0100

    changes to home, footer, navbar, added logo

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -7,6 +7,6 @@ export const HOME_ROUTE: Route = {
   component: HomeComponent,
   data: {
     authorities: [],
-    pageTitle: 'Welcome, Java Hipster!',
+    pageTitle: 'Witamy na stronie o grze HUMANKIND!',
   },
 };
